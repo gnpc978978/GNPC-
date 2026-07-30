6ed99123a1dd85ec1c05ce00d64fd56d4eb5c973
gnpc978978 <gnpc.itops@gmail.com>
2026-07-30 23:36:59 +0530

    Refactor metadata and viewport in layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -63,6 +63,10 @@ export const metadata: Metadata = {
 };
 
 export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  viewportFit: "cover",
   themeColor: "#0F4C81",
 };
 
